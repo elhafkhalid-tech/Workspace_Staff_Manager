d93d0eb09950c1eea4c201e6edd1ea1b1dfdb0d9
elhafkhalid-tech <elhaf.khalid@gmail.com>
2025-11-21 00:05:47 +0100

    ajoute le modale d'information

--- Workspace.js
+++ Workspace.js
@@ -1,16 +1,18 @@
-const workers = [];
-const newWorker = {};
+
+// array global
+const workers = getData();
+
 // get buttons
 const addNewWorker = document.getElementById("AddNewWorker");
 const closeForm = document.getElementById("close");
 const addExperience = document.getElementById("Add_experience");
 const valider = document.getElementById("valider");
-const view = document.getElementById("view");
+
 // get other elements
 const container_unssigned_workers = document.getElementById(
   "container-unssigned-workers"
 );
-const container_experiences = document.getElementById("experience-container");
+const container_experiences = document.getElementById("container-experience");
 const conference = document.getElementById("conference");
 const reception = document.getElementById("reception");
 const service = document.getElementById("service");
@@ -21,79 +23,118 @@ const form = document.getElementById("container-form");
 const photoUrl = document.getElementById("url-img");
 const imgAddWorker = document.getElementById("img-add-worker");
 const img_url = document.getElementById("img-url");
+
 // inputs
 const inputUrl = document.getElementById("url");
 const inputName = document.getElementById("input-name");
 const inputRole = document.getElementById("select-role");
 const inputEmail = document.getElementById("email");
 const inputPhone = document.getElementById("phone");
-const inputCompany = document.getElementById("company");
-const inputRoleExp = document.getElementById("exp-role");
-const inputFrom = document.getElementById("from");
-const inputTo = document.getElementById("to");
 
-function AfficherViews() {}
+setAsideFull();
 
-function getNewWorker()
-{
-  const img = inputUrl.value;
-  const name = inputName.value;
-  const role = inputRole.value;
-  const email = inputEmail.value;
-  const phone = inputPhone.value;
-  const company = inputCompany.value;
-  const exp_role = inputRoleExp.value;
-  const from = inputFrom.value;
-  const to = inputTo.value;
-    newWorker = {
-    img,
-    name,
-    role,
-    email,
-    phone,
-    company,
-    exp_role,
-    from,
-    to,
-  };
+function getData() {
+  let workerData = localStorage.getItem("myLocal");
+  return workerData ? JSON.parse(workerData) : [];
 }
 
-function createUnssigned(img, name, role) {
-  const div_unssigned = document.createElement("div");
-  container_unssigned_workers.innerHTML += ` <div class="unssigned-article">
-            <article class="article">
-              <div class="img-nom-role">
-                <img id="img" src="${img}" alt="Img" />
-                <div>
-                  <p>${name}</p>
-                  <span>${role}</span>
-                </div>
-              </div>
-              <button id="view">view</button>
-            </article>
-          </div> `;
-  // container_unssigned_workers.append(div_unssigned);
+function setAsideFull()
+{ 
+    workers.forEach((worker, index) => {
+    createUnssigned(worker, index);
+    const view = document.getElementById(index);
+    view.addEventListener("click", () => {
+      afficherView(worker);
+    });
+  });
 }
 
+// Show worker details
+function afficherView(worker) {
+  const container_details = document.getElementById("view-details");
+  container_details.innerHTML = `
+    <div id="buttonView" class = "buttonView">
+      <button>X</button>
+    </div>
+    <div class="worker-modal">
+      <img src="${worker.img}" class="worker-img">
+      <div class="worker-info">
+        <h2>${worker.name}</h2>
+        <p><b>Role : </b>${worker.role}</p>
+        <p><b>Email : </b>${worker.email}</p>
+        <p><b>Phone : </b>${worker.phone}</p>
+      </div>
+    </div>
+    <div id="exp-worker" ">
+       
+    </div>
+  `;
+  // console.log(worker.experiences);
+  worker.experiences.forEach((ex) => {
+    const exp_worker = document.getElementById("exp-worker");
+    const div_each_experience = document.createElement("div");
+    div_each_experience.classList = "exp-worker";
+    div_each_experience.innerHTML += ` 
+          <p> <b>company : </b> ${ex.company} </p> 
+          <p> <b>role :</b> ${ex.role_exp} </p> 
+          <p> <b>from : </b> ${ex.from} </p> 
+          <p> <b>to : </b> : ${ex.to} </p>  
+    `;
+    exp_worker.append(div_each_experience);
+  });
+
+  container_details.style.display = "block";
+  const buttonView = document.getElementById("buttonView");
+  buttonView.addEventListener("click", () => {
+    container_details.style.display = "none";
+  });
+}
+
+// Create unassigned worker
+function createUnssigned(worker, index) {
+  const div = document.createElement("div");
+  div.className = "unssigned-article";
+  div.innerHTML = `
+    <article class="article">
+      <div class="img-nom-role">
+        <img id="img" src="${worker.img}" alt="Img" />
+        <div>
+          <p>${worker.name}</p>
+          <span>${worker.role}</span>
+        </div>
+      </div>
+      <button class="view-btn" id="${index}">view</button>
+    </article>
+  `;
+  container_unssigned_workers.appendChild(div);
+}
+
+// Create assigned worker
 function createAssigned(room) {
   const div_assigned = document.createElement("div");
-  room.innerHTML += `<div class="assigned-article">
-                <article class="article">
-                  <img src="img/test.png" alt="Img" />
-                  <div>
-                    <p>nom</p>
-                    <span>role</span>
-                  </div>
-                </article>
-                <button>X</button>
-              </div>`;
+  room.innerHTML += `
+    <div class="assigned-article">
+      <article class="article">
+        <img src="img/test.png" alt="Img" />
+        <div>
+          <p>nom</p>
+          <span>role</span>
+        </div>
+      </article>
+      <button>X</button>
+    </div>
+  `;
 }
 
+// Create experience
 function createExperience() {
-  container_experiences.innerHTML += `
+  container_experiences.innerHTML += ` <div class = "experience-form" id="experience-form"> 
+    <div class = "close_experience"> 
+      <button>X</button>
+    </div>
     <div class="modal">
       <label>Company</label>
-      <input id="company" type="text"  />
+      <input id="company" type="text" />
     </div>
     <div class="modal">
       <label>Role</label>
@@ -107,10 +148,13 @@ function createExperience() {
       <label>To</label>
       <input id="to" type="date" />
     </div>
+    </div>
     </br>
-   `;
+  `;
+  const close_experience = document.getElementById();
 }
 
+// Events
 addNewWorker.addEventListener("click", () => {
   form.style.display = "block";
   inputUrl.value = "";
@@ -130,14 +174,35 @@ inputUrl.addEventListener("input", () => {
 });
 
 valider.addEventListener("click", () => {
-  getNewWorker();
-  workers.push(newWorker);
-  if (name !== "" && img !== "" && role !== "") {
-    createUnssigned(img, name, role);
-  }
-  form.style.display = "none";
-});
+  const name = inputName.value;
+  const img = inputUrl.value;
+  const role = inputRole.value;
+  const email = inputEmail.value;
+  const phone = inputPhone.value;
+  // const id = Math.random();
 
-view.addEventListener("click", () => {
-  AfficherViews();
+  let experiences = [];
+  const experience_form = document.querySelectorAll(".experience-form");
+  
+  experience_form.forEach(() => {
+    const company = document.getElementById("company").value;
+    const role_exp = document.getElementById("exp-role").value;
+    const from = document.getElementById("from").value;
+    const to = document.getElementById("to").value;
+    experiences.push({ company, role_exp, from, to });
+  });
+  
+  const worker = { img, name, role, email, phone, experiences };
+  workers.push(worker);
+  localStorage.setItem('myLocal',JSON.stringify(workers));
+  
+  if (img && name && role && email && phone)
+    createUnssigned(worker, workers.length - 1);
+
+  const view = document.getElementById(workers.length - 1);
+  view.addEventListener("click", () => {
+    afficherView(worker);
+  });
+  
+  form.style.display = "none";
 });
